69d097edac07d0e6aa3fe98572ffb7ef4316d3e5
Bright Anyawe <brightanyawe55@gmail.com>
2024-12-16 09:18:48 +0000

    Improve the desgin styles on mobile

---
+++ .history/src/main_20241215070855.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './Styles/index.css'
+// import "./Styles/SideMessage.css";
+import "./Styles/App.css";
+import "./Styles/Design.css";
+import "./Styles/Design_Responsiveness.css";
+
+
+
+import App from './App'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
