36c11c6fc8ed6e2b780f92fde059f0c9e3762a90
Bright Anyawe <brightanyawe55@gmail.com>
2025-01-18 13:31:03 +0000

    Add terser and cssnano to project
    This is for better optimization

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: "jsdom",
-    setupFiles: "src/Test/setup.js",
+    environment: 'jsdom',
+    setupFiles: 'src/Test/setup.js',
+  },
+  build: {
+    minify: 'terser', 
+    terserOptions: {
+      format: {
+        comments: false, 
+      },
+      compress: {
+        drop_console: true,
+      },
+      mangle: true, 
+    },
   },
 });
